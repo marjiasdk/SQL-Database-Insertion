0e3155e19558b499d4cd446502b783c3e6edf923
Marjia Siddik <marjia.siddik3@mail.dcu.ie>
2023-06-04 20:47:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+#!"[location of Python Application]"
+print("Content-Type: text/html\n\n")
+
+import cgi
+form=cgi.FieldStorage()
+id=form.getvalue("id")
+email=form.getvalue("email")
+
+import sys
+sys.path.append("[Location of SQL Connector]")
+import mysql.connector
+
+con=mysql.connector.connect(user='root', password='', host='localhost', database='[database name]')
+cur=con.cursor()
+
+cur.execute("insert into test1 values(%s,%s)", (id,email))
+con.commit()
+
+cur.close()
+con.close()
+
+print("<h3>Your record has been inserted into the SQL Database successfuly.</h3>")
+print("<a href='http://localhost/index.html'><h3>Click here to return to the website!</h3></a>")
